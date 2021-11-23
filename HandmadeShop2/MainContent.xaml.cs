72747d9e88f777912ccbdafaa688f14d838646f2
Гульнара Гарифинова <gulnaragarifinova@yandex.ru>
2021-11-23 20:26:10 +0300

    add photo in database

--- HandmadeShop2/MainContent.xaml.cs
+++ HandmadeShop2/MainContent.xaml.cs
@@ -19,6 +19,7 @@ namespace HandmadeShop2
     /// </summary>
     public partial class MainContent : Window
     {
+        public int x = 1;
         public MainContent()
         {
             InitializeComponent();
@@ -27,11 +28,35 @@ namespace HandmadeShop2
 
         private void GetInfo()
         {
-            var lst = MainWindow.dba.Product;
-            foreach (var item in lst)
+            tblock_1.Text = "";
+            tblock2.Text = "";
+            tblock3.Text = "";
+            tblock4.Text = "";
+            db.Product product = new db.Product();
+            try
             {
-                tblock_1.Text = item.Name + "\n" + item.Price.ToString();
+                var lst1 = from cust in MainWindow.dba.Product
+                           where cust.ID_product == x
+                           select cust ; 
+                
+                var lst2 = from cust in MainWindow.dba.Product
+                          where cust.ID_product == x+1
+                          select cust; 
+                
+                var lst3 = from cust in MainWindow.dba.Product
+                          where cust.ID_product == x+2
+                          select cust; 
+                
+                var lst4 = from cust in MainWindow.dba.Product
+                          where cust.ID_product == x+3
+                          select cust;
+
+                tblock_1.Text = lst1.First().Name + "\n" + lst1.First().Price;
+                tblock2.Text = lst2.First().Name + "\n" + lst2.First().Price;
+                tblock3.Text = lst3.First().Name + "\n" + lst3.First().Price;
+                tblock4.Text = lst4.First().Name + "\n" + lst4.First().Price;
             }
+            catch { }
         }
 
         private void Back_Click(object sender, RoutedEventArgs e)
@@ -51,6 +76,32 @@ namespace HandmadeShop2
         private void Close_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void Forward_Click(object sender, RoutedEventArgs e)
+        {
+            if (x+4 <= MainWindow.dba.Product.Count<db.Product>())
+            {
+                x +=4;
+            }
+            else
+            {
+                x = 1;
+            }
+            GetInfo();
+        }
+
+        private void Prev_Click(object sender, RoutedEventArgs e)
+        {
+            if (x - 4 > 0)
+            {
+                x -=4;
+            }
+            else
+            {
+                x = MainWindow.dba.Product.Count<db.Product>();
+            }
+            GetInfo();
         }
     }
 }
